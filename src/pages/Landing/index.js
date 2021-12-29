586ca95791acd14324d0ce90d959809d9e08daa9
KevenCavalcante <kecr97@gmail.com>
2021-12-29 07:08:00 -0300

    'download-app'

--- src/pages/Landing/index.js
+++ src/pages/Landing/index.js
@@ -12,7 +12,7 @@ function Landing() {
                     class="flex flex-col justify-center text-center bg-spotify-theme-mobile py-40 px-4 md:bg-spotify-theme bg-banner-mobile md:bg-banner bg-195% md:bg-175% bg-gradient-to-r from-gradiente2 to-gradiente1 text-white">
                     <h1 class="text-5xl md:text-9xl mb-10 font-bold max-w-4xl mx-auto">Treinar Muda Tudo</h1>
                     <p class="text-md md:text-lg mb-10">Exercicios e acompanhamento profissional em todo seu percurso!</p>
-                    <a href="#" class="bg-color-buttons hover:bg-white text-gradiente1 text-sm rounded-full px-10 py-3 uppercase font-bold max-w-xl mx-auto tracking-widest transition duration-500">Baixe o App Agora!</a>
+                    <a href="https://exp-shell-app-assets.s3.us-west-1.amazonaws.com/android/%40kevenrodriigues/Vo2MaX-e29e8752242f40549b933ccf186e466d-signed.apk" class="bg-color-buttons hover:bg-white text-gradiente1 text-sm rounded-full px-10 py-3 uppercase font-bold max-w-xl mx-auto tracking-widest transition duration-500">Baixe o App Agora!</a>
                 </div>
             </main>
             <Footer />
